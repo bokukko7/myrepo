8c90ffd3068cc09a045509bef49a843a5af82872
bokukko <bokukko7@gmail.com>
2017-10-21 18:51:44 +0900

    2nd commit

---
+++ ValueType/ValueType/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ValueType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 100;
+            Console.WriteLine(x);
+
+            Value v;
+            v = new Value();
+            v.value = 1;
+
+            Value u;
+            u = new Value();
+
+            Value w;
+            w = v;
+            w.value = 10;
+
+            Console.WriteLine(v.value);
+            Console.WriteLine(u.value);
+            Console.WriteLine(w.value);
+        }
+    }
+
+    class Value
+    {
+        public int value;
+    }
+}
